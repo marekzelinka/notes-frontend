f1495d4481a39907e342f374a78de16390fb138e
Marek Zelinka <mzelinka17@gmail.com>
2024-07-22 12:54:16 +0200

    rendering collections

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ function App({ notes }) {
     <>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map((note) => (
+          <li>{note.content}</li>
+        ))}
       </ul>
     </>
   )
