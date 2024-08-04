0fa60aae1ce5cc02911ee5e732c6005d00dfa6c7
Marek Zelinka <mzelinka17@gmail.com>
2024-08-04 17:18:30 +0200

    test clicking toggle button

--- src/components/Note.test.jsx
+++ src/components/Note.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
-import { test } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import { expect, test, vi } from 'vitest'
 import { Note } from './Note'
 
 test('renders content', () => {
@@ -10,4 +11,19 @@ test('renders content', () => {
 
   render(<Note note={note} />)
   screen.getByText(note.content)
+})
+
+test('clicking the toggle button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true,
+  }
+
+  const mockOnToggleImportance = vi.fn()
+
+  render(<Note note={note} onToggleImportance={mockOnToggleImportance} />)
+
+  const user = userEvent.setup()
+  await user.click(screen.getByRole('button', { name: /make not important/i }))
+  expect(mockOnToggleImportance.mock.calls).toHaveLength(1)
 })
